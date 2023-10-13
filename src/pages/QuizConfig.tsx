858e376ca9f02f7c997a71cf6499f54326d0cc68
Max Carvajal <jcarvajal288@gmail.com>
2023-10-12 22:31:13 -0500

    Add prototype UI

--- src/pages/QuizConfig.tsx
+++ src/pages/QuizConfig.tsx
@@ -1,5 +1,34 @@
-import { Typography } from "@mui/material"
+import { Button, Checkbox, FormControlLabel, FormGroup, Paper, Stack, TextField, Typography } from '@mui/material';
 
 export const QuizConfig = () => (
-    <Typography>Hello world</Typography>
-)
+    <>
+        <Typography variant='h2'>Configure Quiz</Typography>
+        <Stack>
+            <TextField
+                id='api-key-input'
+                label='API Key'
+                variant='outlined'
+            />
+            <Paper>
+                <Stack direction='column'>
+                    <Typography variant='h5'>Item Types</Typography>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Checkbox />}
+                            label={'Radicals'}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox />}
+                            label={'Kanji'}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox />}
+                            label={'Vocabulary'}
+                        />
+                    </FormGroup>
+                </Stack>
+            </Paper>
+            <Button variant='contained'>Generate Quiz</Button>
+        </Stack>
+    </>
+);
--- src/pages/__tests/QuizConfig.test.tsx
+++ src/pages/__tests/QuizConfig.test.tsx
@@ -1,10 +1,29 @@
 /** @jest-environment jsdom */
-import { render, screen } from "@testing-library/react";
-import {QuizConfig} from "../QuizConfig.tsx";
+import { render, screen } from '@testing-library/react';
+import { QuizConfig } from '../QuizConfig.tsx';
+import { beforeEach } from 'vitest';
 
 describe('QuizConfig', () => {
-    it('has a hello world', () => {
-        render(<QuizConfig/>)
-        expect(screen.getByText("Hello world")).toBeVisible();
-    })
+    beforeEach(() => {
+        render(<QuizConfig />);
+    });
+
+    it('has a title', () => {
+        expect(screen.getByText('Configure Quiz')).toBeVisible();
+    });
+
+    it('has an API key input', () => {
+        expect(screen.getByRole('textbox', { name: 'API Key' }));
+    });
+
+    it('shows type selectors', () => {
+        expect(screen.getByText('Item Types'));
+        expect(screen.getByText('Radicals'));
+        expect(screen.getByText('Kanji'));
+        expect(screen.getByText('Vocabulary'));
+    });
+
+    it('shows Generate Quiz button', () => {
+        expect(screen.getByRole('button', { name: 'Generate Quiz' }));
+    });
 });
