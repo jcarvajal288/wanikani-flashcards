273e75bb4c33b5c8a13e04f1d61da73447870f64
Max Carvajal <jcarvajal288@gmail.com>
2023-10-13 21:36:45 -0500

    Read checkbox values from form

--- src/pages/QuizConfig.tsx
+++ src/pages/QuizConfig.tsx
@@ -1,34 +1,73 @@
 import { Button, Checkbox, FormControlLabel, FormGroup, Paper, Stack, TextField, Typography } from '@mui/material';
+import { FormEvent, useState } from 'react';
 
-export const QuizConfig = () => (
-    <>
-        <Typography variant='h2'>Configure Quiz</Typography>
-        <Stack>
-            <TextField
-                id='api-key-input'
-                label='API Key'
-                variant='outlined'
-            />
-            <Paper>
-                <Stack direction='column'>
-                    <Typography variant='h5'>Item Types</Typography>
-                    <FormGroup>
-                        <FormControlLabel
-                            control={<Checkbox />}
-                            label={'Radicals'}
-                        />
-                        <FormControlLabel
-                            control={<Checkbox />}
-                            label={'Kanji'}
-                        />
-                        <FormControlLabel
-                            control={<Checkbox />}
-                            label={'Vocabulary'}
-                        />
-                    </FormGroup>
+type FormData = {
+    radicals: boolean;
+    kanji: boolean;
+    vocabulary: boolean;
+};
+
+export const QuizConfig = () => {
+    const [formData, setFormData] = useState<FormData>({
+        radicals: false,
+        kanji: false,
+        vocabulary: false,
+    });
+
+    const setFormValue = (field: Partial<FormData>) => {
+        setFormData({
+            ...formData,
+            ...field,
+        });
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        console.log(JSON.stringify(formData));
+        e.preventDefault();
+    };
+
+    return (
+        <>
+            <Typography variant='h2'>Configure Quiz</Typography>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <Stack>
+                    <TextField
+                        id='api-key-input'
+                        label='API Key'
+                        variant='outlined'
+                    />
+                    <Paper>
+                        <Stack direction='column'>
+                            <Typography variant='h5'>Item Types</Typography>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox onChange={(e) => setFormValue({ radicals: e.target.checked })} />
+                                    }
+                                    label={'Radicals'}
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox onChange={(e) => setFormValue({ kanji: e.target.checked })} />}
+                                    label={'Kanji'}
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox onChange={(e) => setFormValue({ vocabulary: e.target.checked })} />
+                                    }
+                                    label={'Vocabulary'}
+                                />
+                            </FormGroup>
+                        </Stack>
+                    </Paper>
+                    <Button
+                        variant='contained'
+                        type='submit'
+                    >
+                        Generate Quiz
+                    </Button>
                 </Stack>
-            </Paper>
-            <Button variant='contained'>Generate Quiz</Button>
-        </Stack>
-    </>
-);
+            </form>
+            <Typography>{JSON.stringify(formData)}</Typography>
+        </>
+    );
+};
