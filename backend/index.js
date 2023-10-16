9d0ab2851cb8588f4774320945351f467deb6601
Max Carvajal <jcarvajal288@gmail.com>
2023-10-15 19:29:44 -0500

    Successfully post subject data to backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const jsonParser = bodyParser.json({ limit: '2mb' });
+
 app.use(cors());
 
-app.post('/fillDatabase', (request, response) => {
+app.post('/fillDatabase', jsonParser, (request, response) => {
     const subjects = request.body;
     console.log(`Filling database with ${subjects.length} subjects`);
 });
