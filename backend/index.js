0713214768f5e6c61ed487764173c9a8d4170539
Max Carvajal <jcarvajal288@gmail.com>
2023-10-17 21:56:53 -0500

    Inserted a single subject into the database

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+import bodyParser from 'body-parser';
+
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 const jsonParser = bodyParser.json({ limit: '2mb' });
 
+const databaseUri = 'mongodb://root:example@localhost:27017/?authMechanism=DEFAULT';
+const databaseClient = new MongoClient(databaseUri);
+
 app.use(cors());
 
-app.post('/fillDatabase', jsonParser, (request, response) => {
+app.post('/fillDatabase', jsonParser, async (request, response) => {
     const subjects = request.body;
     console.log(`Filling database with ${subjects.length} subjects`);
+    const database = databaseClient.db('insertDB');
+    const subjectsTable = database.collection('subjects');
+    const result = await subjectsTable.insertOne(subjects[0]);
+    console.log('inserted id ' + result.insertedId);
 });
 
 app.listen(PORT, () => {
