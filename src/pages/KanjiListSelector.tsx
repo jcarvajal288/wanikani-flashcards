624e7c2602e65c945fa9f038e6becf772578ba55
Max Carvajal <john.m.carvajal.ctr@army.mil>
2024-05-31 15:17:07 -0500

    Extract out JLPT selector

---
+++ src/pages/KanjiListSelector.tsx
@@ -0,0 +1,90 @@
+import {Button, ButtonGroup, ClickAwayListener, Grow, MenuItem, MenuList, Paper, Popper, Stack} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import {Dispatch, useRef, useState} from "react";
+
+type KanjiListSelectorProps = {
+  options: string[];
+  kanjiLists: number[][];
+  setQuizItems: Dispatch<number[]>;
+}
+
+export const KanjiListSelector = (props: KanjiListSelectorProps) => {
+
+  const [selectedOption, setSelectedOption] = useState<number>(0);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
+
+  const handleDropdownClose = (event: Event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return;
+    }
+    setDropdownOpen(false);
+  };
+
+  const handleMenuItemClick = (_event: React.MouseEvent<HTMLLIElement, MouseEvent>, index: number) => {
+    setSelectedOption(index);
+    setDropdownOpen(false);
+  };
+
+  const handleSubmit = () => {
+    props.setQuizItems(props.kanjiLists[selectedOption]);
+  };
+
+  return (
+    <Stack>
+      <ButtonGroup
+        variant='contained'
+        ref={anchorRef}
+      >
+        <Button fullWidth onClick={handleSubmit}>{props.options[selectedOption]}</Button>
+        <Button
+          size='small'
+          data-testid='jlpt-selector'
+          aria-controls={dropdownOpen ? 'split-button-menu' : undefined}
+          aria-expanded={dropdownOpen ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={() => {
+            setDropdownOpen((prevOpen) => !prevOpen)
+          }}
+        >
+          <ArrowDropDownIcon/>
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{zIndex: 1}}
+        open={dropdownOpen}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({TransitionProps, placement}) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleDropdownClose}>
+                <MenuList autoFocusItem>
+                  {props.options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      selected={index === selectedOption}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </Stack>
+  )
+}
--- src/pages/MiscellaneousSelectors.tsx
+++ src/pages/MiscellaneousSelectors.tsx
@@ -1,17 +1,13 @@
+import {Checkbox, FormControlLabel, FormGroup, Paper, Stack, Typography} from '@mui/material';
+import {ChangeEvent, Dispatch} from 'react';
 import {
-    Button,
-    ButtonGroup,
-    Checkbox, ClickAwayListener,
-    FormControlLabel,
-    FormGroup, Grow, MenuItem, MenuList,
-    Paper,
-    Popper,
-    Stack,
-    Typography
-} from '@mui/material';
-import React, {ChangeEvent, Dispatch, useRef, useState} from 'react';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import {JLPT_N1_KANJI_IDS, JLPT_N2_KANJI_IDS, JLPT_N3_KANJI_IDS, JLPT_N4_KANJI_IDS, JLPT_N5_KANJI_IDS} from "../assets/kanjiLists.tsx";
+    JLPT_N1_KANJI_IDS,
+    JLPT_N2_KANJI_IDS,
+    JLPT_N3_KANJI_IDS,
+    JLPT_N4_KANJI_IDS,
+    JLPT_N5_KANJI_IDS
+} from "../assets/kanjiLists.tsx";
+import {KanjiListSelector} from "./KanjiListSelector.tsx";
 
 interface ItemTypesParams {
     toggleCriticalCondition: (e: ChangeEvent<HTMLInputElement>) => void;
@@ -20,42 +16,6 @@ interface ItemTypesParams {
 
 export const MiscellaneousSelectors = (props: ItemTypesParams) => {
 
-    const jlptOptions = ['JLPT5', "JLPT4", "JLPT3", "JLPT2", "JLPT1"];
-    const [selectedJLPT, setSelectedJLPT] = useState<number>(0);
-    const [jlptOpen, setJlptOpen] = useState<boolean>(false);
-    const anchorRef = useRef<HTMLDivElement>(null);
-
-    const handleJLPTClose = (event: Event) => {
-        if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
-            return;
-        }
-        setJlptOpen(false);
-    };
-
-    const handleJLPTMenuItemClick = (_event: React.MouseEvent<HTMLLIElement, MouseEvent>, index: number) => {
-        setSelectedJLPT(index);
-        setJlptOpen(false);
-    };
-
-    const handleJLPTClick = async () => {
-        switch(jlptOptions[selectedJLPT]) {
-            case 'JLPT5':
-                props.setQuizItems(JLPT_N5_KANJI_IDS);
-                break;
-            case 'JLPT4':
-                props.setQuizItems(JLPT_N4_KANJI_IDS);
-                break;
-            case 'JLPT3':
-                props.setQuizItems(JLPT_N3_KANJI_IDS);
-                break;
-            case 'JLPT2':
-                props.setQuizItems(JLPT_N2_KANJI_IDS);
-                break;
-            case 'JLPT1':
-                props.setQuizItems(JLPT_N1_KANJI_IDS);
-                break;
-        }
-    };
 
     return (
         <Paper>
@@ -70,59 +30,11 @@ export const MiscellaneousSelectors = (props: ItemTypesParams) => {
                         label={'Critical Condition'}
                     />
                 </FormGroup>
-                <ButtonGroup
-                    variant='contained'
-                    ref={anchorRef}
-                >
-                    <Button fullWidth onClick={handleJLPTClick}>{jlptOptions[selectedJLPT]}</Button>
-                    <Button
-                        size='small'
-                        data-testid='jlpt-selector'
-                        aria-controls={jlptOpen ? 'split-button-menu' : undefined}
-                        aria-expanded={jlptOpen ? 'true' : undefined}
-                        aria-label="select merge strategy"
-                        aria-haspopup="menu"
-                        onClick={() => {
-                            setJlptOpen((prevOpen) => !prevOpen)
-                        }}
-                    >
-                        <ArrowDropDownIcon/>
-                    </Button>
-                </ButtonGroup>
-                <Popper
-                    sx={{zIndex: 1}}
-                    open={jlptOpen}
-                    anchorEl={anchorRef.current}
-                    role={undefined}
-                    transition
-                    disablePortal
-                >
-                  {({ TransitionProps, placement }) => (
-                    <Grow
-                      {...TransitionProps}
-                      style={{
-                        transformOrigin:
-                          placement === 'bottom' ? 'center top' : 'center bottom',
-                      }}
-                    >
-                    <Paper>
-                        <ClickAwayListener onClickAway={handleJLPTClose}>
-                            <MenuList autoFocusItem>
-                                {jlptOptions.map((option, index) => (
-                                    <MenuItem
-                                        key={option}
-                                        selected={index === selectedJLPT}
-                                        onClick={(event) => handleJLPTMenuItemClick(event, index)}
-                                    >
-                                        {option}
-                                    </MenuItem>
-                                ))}
-                            </MenuList>
-                        </ClickAwayListener>
-                    </Paper>
-                    </Grow>
-                    )}
-                </Popper>
+                <KanjiListSelector
+                  options={['JLPT5', "JLPT4", "JLPT3", "JLPT2", "JLPT1"]}
+                  kanjiLists={[JLPT_N5_KANJI_IDS, JLPT_N4_KANJI_IDS, JLPT_N3_KANJI_IDS, JLPT_N2_KANJI_IDS, JLPT_N1_KANJI_IDS]}
+                  setQuizItems={props.setQuizItems}
+                />
             </Stack>
         </Paper>
     )
